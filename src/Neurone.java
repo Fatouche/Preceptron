0ff3d33406bd87608f6ea164ba6d5a646b3c3dd8
fatouche <foucaud.giovanny@gmail.com>
2018-03-08 11:33:27 +0100

    12

--- src/Neurone.java
+++ src/Neurone.java
@@ -1,24 +1,40 @@
-
+import java.util.Vector;
 
 public class Neurone {
 	private final double pas = 0.01;
 	
 	private double biais;
 	private int sortie;
-	private double poids[];
+	private Vector<Double> poids;
 	
 	public Neurone(){
 		this.sortie = 0;
 		this.biais = 0.5;
 		for(int i = 0; i < 2; i++ ){
-			this.poids[i] = Math.random(); 
+			this.poids.add(Math.random()); 
 		}
 	}
 	
-	public Neurone(double diais, int sortie, double poids[]){
+	public Neurone(double biais, int sortie, Vector<Double> poids){
 		this.biais = biais;
 		this.poids = poids;
 		this.sortie = sortie;
+	}
+	
+	public int calculNeurone(Neurone n){
+		double res = 0 ;
+		
+		for(int i = 0; i < n.poids.size(); i = i+2){
+			res += (n.poids.elementAt(i) * n.poids.elementAt(i+1)) - n.biais;  
+		}
+		
+		if(res > 0){
+			this.sortie = 0;
+		}else{
+			this.sortie = -1;
+		}
+		
+		return sortie;
 	}
 
 }
