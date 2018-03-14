c30d1945a764dceea1b7081b1e5eba7b682fea2d
fatouche <foucaud.giovanny@gmail.com>
2018-03-14 16:26:23 +0100

    Fin perceptron

--- src/Neurone.java
+++ src/Neurone.java
@@ -1,11 +1,13 @@
 import java.util.Vector;
 
 public class Neurone {
+	
 	private final double pas = 0.01;
 	
 	private double biais;
 	private int sortie;
-	private Vector<Double> poids;
+	private Vector<Double> poids = new Vector<>();
+	private int nbErreur = 0;
 	
 	public Neurone(){
 		this.sortie = 0;
@@ -21,20 +23,39 @@ public class Neurone {
 		this.sortie = sortie;
 	}
 	
-	public int calculNeurone(Neurone n){
-		double res = 0 ;
+	public int calculNeurone(Point p){
+		double sigma = 0 ;
 		
-		for(int i = 0; i < n.poids.size(); i = i+2){
-			res += (n.poids.elementAt(i) * n.poids.elementAt(i+1)) - n.biais;  
-		}
+		sigma += ((this.poids.elementAt(0) * p.getX()) + (this.poids.elementAt(1) * p.getY())) - this.biais;  
 		
-		if(res > 0){
-			this.sortie = 0;
+		if(sigma > 0){
+			this.sortie = 1;
 		}else{
 			this.sortie = -1;
 		}
 		
-		return sortie;
+		return this.sortie;
+	}
+	
+	public void MAJNeurone(Point p){
+		double add1 = this.pas * (p.getTag()-this.sortie)*p.getX();
+		double add2 = this.pas * (p.getTag()-this.sortie)*p.getY();
+		
+		this.biais += this.pas * ( p.getTag()- this.sortie)*(-0.5);
+		
+		this.poids.set(0, this.poids.get(0)+add1);
+		this.poids.set(1, this.poids.get(1)+add2);
+	}
+	
+	public void run(Vector<Point> points){
+		nbErreur = 0;
+		for (Point point : points) {
+			if(this.calculNeurone(point) != point.getTag()){
+				nbErreur++;
+				this.MAJNeurone(point);
+			}
+		}
+		System.out.println(" le nombre d'erreur(s) : " + nbErreur);
 	}
 
 }
--- src/Perceptron.java
+++ src/Perceptron.java
@@ -13,6 +13,10 @@ public class Perceptron {
 			x1 = Math.random();
 			x2 = Math.random();
 			
+			while(x2 < 0.5){
+				x2 = Math.random();
+			}
+			
 			if(x1+x2-1 > 0){
 				vPoints.add(new Point(x1,x2,1));
 			}else{
@@ -45,11 +49,21 @@ public class Perceptron {
 	}
 	
 	
+	
+	/* Le pas -> plus on prend un pas élevé, plus de chance de trouvé rapidement la solution mais un nombre d'erreur élevée
+	 * 		     plus on prend un pas petit, plus on est précis, le nombre d'erreurs diminue au fur et à mesure, mais c'est beaucoup plus long a trouvé la solution */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Perceptron p = new Perceptron();
-		Vector<Point> points = p.generationPoint(50);
+		Vector<Point> points = p.generationPoint(10000);
 		p.showVPoints(points);
+		
+		Neurone n = new Neurone();
+		for(int i = 0; i < 100; i++){
+			n.run(points);
+			System.out.print("nombre d'itération : " + i);
+		}
 	}
 
 }
